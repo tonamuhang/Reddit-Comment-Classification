7040de5202e9c5aae40642ec09e0b71c66427932
Muhang <limuhang1@gmail.com>
2019-10-04 19:16:56 -0400

    LR done

--- test.py
+++ test.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import numpy as np
-from sklearn import feature_extraction
-import matplotlib.pyplot as plt
-import matplotlib
-from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.feature_extraction.text import TfidfTransformer
 
 # load data
 train = pd.read_csv("reddit_train.csv", sep=',')
 test = pd.read_csv("reddit_test.csv", sep=',')
 test_x = test['comments']
-# test_y = test['subreddits']
-train_x = train['comments']
-train_y = train['subreddits']
 
+comment = train['comments']
+labels_list = train['subreddits']
+tfidf = TfidfVectorizer(ngram_range=(1, 1), stop_words='english')
+features = tfidf.fit_transform(comment)
+labels = labels_list
 
 # fig = plt.figure(figsize=(8,6))
 # plt.tight_layout()
@@ -28,18 +30,23 @@ train_y = train['subreddits']
 #
 # pipline.fit(train_x, train_y)
 
-# Process data
-print("Processing data")
-cvec = feature_extraction.text.CountVectorizer(ngram_range=(1, 1), stop_words='english')
-train_x = cvec.fit(train_x).transform(train_x)
+# Process Data
+X_train, X_test, y_train, y_test = train_test_split(comment, labels, random_state = 0)
+cvec = CountVectorizer()
+X_train_counts = cvec.fit_transform(X_train)
+X_test_counts = cvec.transform(X_test)
+
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+X_test_tfidf = tfidf_transformer.transform(X_test_counts)
 
 # TODO: Fix y format
 # Start to fit
 print("Starting to fit...")
 clf = LogisticRegression(random_state=0, multi_class='multinomial', solver='newton-cg')
-clf.fit(train_x, train_y)
+clf.fit(X_train_tfidf, y_train)
 
 # Predict
 print("Starting to predict")
-predicted = clf.predict(test_x)
+predicted = clf.predict(X_test_tfidf)
 print(predicted)
