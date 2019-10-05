15554e715ebf9300fead4ab1242d83ef0b1e5cc0
tonamuhang <39136949+tonamuhang@users.noreply.github.com>
2019-10-04 20:05:39 -0400

    Update pipeline
    Follow the pipeline format to do pipeline testing

--- test.py
+++ test.py
@@ -1,52 +1,56 @@
 import pandas as pd
 import numpy as np
+from sklearn import feature_extraction, preprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfTransformer
-
+from sklearn.metrics import accuracy_score
 # load data
+from sklearn.pipeline import Pipeline
+
 train = pd.read_csv("reddit_train.csv", sep=',')
 test = pd.read_csv("reddit_test.csv", sep=',')
 test_x = test['comments']
 
 comment = train['comments']
 labels_list = train['subreddits']
-tfidf = TfidfVectorizer(ngram_range=(1, 1), stop_words='english')
-features = tfidf.fit_transform(comment)
+
 labels = labels_list
-
-# fig = plt.figure(figsize=(8,6))
-# plt.tight_layout()
-# train.groupby('subreddits').subreddits.count().plot.bar(ylim=0)
-# plt.show()
-
-# Pipline format, follow this:
-# pipline = Pipeline([('clf', LogisticRegression(C=10, multi_class='multinomial')),
-#                     ('vect', feature_extraction.text.CountVectorizer(ngram_range=(1, 2), stop_words='english'),
-#                      ('tfidf', feature_extraction.text.TfidfTransformer()))])
-#
-# pipline.fit(train_x, train_y)
 
 # Process Data
 X_train, X_test, y_train, y_test = train_test_split(comment, labels, random_state = 0)
-cvec = CountVectorizer()
+cvec = CountVectorizer(min_df= 1, stop_words='english')
 X_train_counts = cvec.fit_transform(X_train)
 X_test_counts = cvec.transform(X_test)
 
-tfidf_transformer = TfidfTransformer()
+tfidf_transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 X_test_tfidf = tfidf_transformer.transform(X_test_counts)
 
-# TODO: Fix y format
-# Start to fit
-print("Starting to fit...")
-clf = LogisticRegression(random_state=0, multi_class='multinomial', solver='newton-cg')
-clf.fit(X_train_tfidf, y_train)
+# # TODO: Fix y format
+# # Start to fit
+# print("Starting to fit...")
+# clf = LogisticRegression(random_state=0, multi_class='multinomial', solver='newton-cg')
+# clf.fit(X_train_tfidf, y_train)
+#
+# # Predict
+# print("Starting to predict")
+# predicted = clf.predict(X_test_tfidf)
+# print(accuracy_score(y_test, predicted))
 
-# Predict
-print("Starting to predict")
-predicted = clf.predict(X_test_tfidf)
-print(predicted)
+# Pipline format, follow this:
+pipeline = Pipeline([('vect', feature_extraction.text.CountVectorizer(ngram_range=(1, 2), stop_words='english')),
+                     ('tfidf', feature_extraction.text.TfidfTransformer()),
+                     ('norm', preprocessing.Normalizer()),
+                     ('clf', LogisticRegression(multi_class='multinomial', solver='newton-cg'))])
+
+parameters = {'clf__C': [1, 2, 5, 10]}
+grid = GridSearchCV(pipeline, param_grid=parameters, cv=5)
+
+grid.fit(X_train, y_train)
+
+print ("score = %3.2f" %(grid.score(X_test, y_test)))
+print (grid.best_params_)
