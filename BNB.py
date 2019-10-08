a9d26346cd9bf83d1b9909e3c20e7fa5458a3eef
tonamuhang <39136949+tonamuhang@users.noreply.github.com>
2019-10-08 04:05:40 -0400

    Updated BNB
    Todo: figure out how to calculate posterior in predict_helper

--- BNB.py
+++ BNB.py
@@ -1,41 +1,115 @@
+import pandas as pd
 import numpy as np
-import math
+from sklearn import feature_extraction, preprocessing, tree
+from sklearn.preprocessing import Normalizer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer, TfidfVectorizer
+import nltk
+from nltk import WordNetLemmatizer, PorterStemmer
+from nltk.corpus import stopwords, wordnet
+from nltk.tokenize import word_tokenize
+from string import punctuation
+import re
+from sklearn.metrics import accuracy_score
 
 class BNB:
 
-    def __init__(self, classes):
-        self.dictionary = set([])
+    def __init__(self):
+        # Probabilities
+        self.likelihood = {}
+        self.prob_matrix = {}
+
+        # Classes
+        self.classes = None
 
     # Input: None
     # Output: None
     # Usage: Create a dictionary
-    def create_dictionary(self):
-        wordset = set([])
-        for sentence in self.X:
-            wordset = wordset.union(set(sentence))
-        self.dictionary = wordset
+    def create_dictionary(self, sentence):
+        sentence_dict = {}
+        prev = ''
+        for word in sentence.split():
+            if word not in sentence_dict:
+                sentence_dict[word] = []
+            sentence_dict[word].append(prev)
+            prev = word
+
+        return sentence_dict
 
     # Input: Dataframe m*n, dataframe n*1
     # Output: None
     def fit(self, feature, label, alpha=1):
         # Load data
         X = feature
-        y = np.array(label)
+        y = label
 
         # Dictionaries for features and classes
-        num_feature = {}
-        num_class = {}
+        self.classes = y.to_frame().subreddits.unique()
+        num_feature = len(X)
+        print(num_feature)
+        num_class = len(self.classes)
 
-        # Probablities
-        prob_class = {}
+        for k in self.classes:
+            # Nk be the total number of documents of that class
+            Nk = len(y[y == k])
+            # nkwt be the number of documents of class k in which wt is observed
+            nkwt = len(X[y == k])
 
-        for i, c in enumerate(label):
-            # Record the class if it hasnt been seen before
-            num_class[c] = num_class.get(c, 0) + 1
+            # Calculate P(y) with laplace smoothing
+            self.likelihood[k] = (alpha + Nk) / (num_class * alpha + num_feature)
 
-            # Record
-            for w in X[i].keys():
-                if w in num_feature:
-                    num_feature[w][c] = num_feature[w].get(c, 0) + 1
-                else:
-                    num_feature[w] = {c: 1}
+            # Calculate P(X|y) with laplace smoothing
+            self.prob_matrix[k] = (alpha + nkwt) / (num_feature * alpha + Nk * num_feature)
+
+        print("Fit Finished")
+        return self
+
+    # Predict on a single class.
+    def predict_helper(self, sentence):
+        result = None
+        features = self.create_dictionary(sentence)
+        max_prob = -999999999
+
+        for k in self.classes:
+            prior = self.prob_matrix[k]
+
+
+            # TODO: Calculate posterior
+            # posterior = [bt P(wt |Ck) + (1−bt) (1−P(wt |Ck))]
+            # P(wt |Ck) = self.likelihood[k]
+            # bt = ???
+            
+            # Probability = prior * posterior
+            # if the new prob is greater than the current max, replace it and the predicted result
+            prob = prior
+            if prob > max_prob:
+                max_prob = prob
+                result = k
+
+        return result
+
+    def predict(self, X):
+        result = {}
+        for sentence in X:
+            result[sentence] = self.predict_helper(sentence)
+
+        return result
+
+    def score(self, X, y):
+        predicted = self.predict(X)
+        # return accuracy_score(y, predicted)
+
+
+# Read in files
+train = pd.read_csv("reddit_train.csv", sep=',')
+comments = train['comments']
+labels = train['subreddits']
+
+# Process Data into train set and test set
+X_train, X_test, y_train, y_test = train_test_split(comments, labels, random_state = 0, test_size=0.5)
+
+bnb = BNB()
+bnb.fit(X_train, y_train)
+
+print(bnb.score(X_test, y_test))
+
