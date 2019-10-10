404dbb998db65f7cb101609af2f80289ceaa6ad8
tonamuhang <39136949+tonamuhang@users.noreply.github.com>
2019-10-10 13:18:16 -0400

    Update textpreprocess.py

--- textpreprocess.py
+++ textpreprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from bs4 import BeautifulSoup
 from sklearn import feature_extraction, preprocessing, tree
 from sklearn.preprocessing import Normalizer
 from sklearn.model_selection import train_test_split, GridSearchCV
@@ -10,6 +11,11 @@ from nltk.corpus import stopwords, wordnet
 from nltk.tokenize import word_tokenize
 from string import punctuation
 import re
+from bs4 import BeautifulSoup
+import urllib
+from urllib.request import urlopen
+from urlextract import URLExtract
+
 # nltk.download('punkt')
 # nltk.download('wordnet')
 # nltk.download('stopwords')
@@ -40,10 +46,27 @@ def set_lowercase(text):
     return text
 
 
-# Remove tags
-def remove_tags(text):
-    text = re.sub("&lt;/?.*?&gt;", " &lt;&gt; ", text)
+def clean_text(text):
+    REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+    BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+    STOPWORDS = set(stopwords.words('english'))
+    extractor = URLExtract()
+
+    for url in extractor.gen_urls(text):
+        try:
+            content = urlopen(url, timeout=3).read()
+            content = BeautifulSoup(content).find('title').string  # HTML decoding
+            text += content
+        except:
+            continue
+
+    text = text.lower()
+    text = REPLACE_BY_SPACE_RE.sub(' ', text)
+    text = BAD_SYMBOLS_RE.sub(' ', text)
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS)
+    print(text)
     return text
+
 
 
 # Remove special characters and digits
@@ -58,54 +81,62 @@ def lemmatize(text):
             text[row_i][word_j] = lemmatizer.lemmatize(text[row_i][word_j])
     return text
 
-def remove_repeat(text):
-    replacer = RepeatReplacer()
-    for row_i in range(text.shape[0]):
-        for word_j in range(len(text[row_i])):
-            text[row_i][word_j] = replacer.replace(text[row_i][word_j])
-    return text
-
 
 class TextPreprocess:
+
+    @staticmethod
+    # Remove tags
+    def remove_tags(text):
+        text = re.sub("&lt;/?.*?&gt;", " &lt;&gt; ", text)
+        return text
+
+    @staticmethod
+    def remove_repeat(text):
+        replacer = RepeatReplacer()
+        for row_i in range(text.shape[0]):
+            for word_j in range(len(text[row_i])):
+                text[row_i][word_j] = replacer.replace(text[row_i][word_j])
+        return text
+
     @staticmethod
     # Input: File location train(str)
     # Output: Preprocessed file(dataframe)
     def process(X):
         # Read in files
         train = pd.read_csv(X, sep=',')
-        test = pd.read_csv("reddit_test.csv", sep=',')
-
-        comments = train['comments']
-        test_x = test['comments']
-        labels = train['subreddits']
-
-        # Note: Order of remove operations matters!
-        comments = comments.apply(lambda x: remove_URLs(x))
-        comments = comments.apply(lambda x: set_lowercase(x))
-        comments = comments.apply(lambda x: remove_tags(x))
-        comments = comments.apply(lambda x: remove_special_chars_digits(x))
-        print("----remove urls tags, set lowercase, remove specials characters and digits----\n", comments.head(5))
-
-        # Tokenize comments
-        comments = comments.apply(word_tokenize)
-        print("----tokenized----\n", comments.head(12))
-
+        #
+        # remove_tags = np.vectorize(TextPreprocess.remove_tags)
+        # remove_repeat = np.vectorize(TextPreprocess.remove_repeat)
+        #
+        # # Note: Order of remove operations matters!
+        # comments = comments.apply(lambda x: remove_tags(x))
+        # comments = comments.apply(lambda x: remove_special_chars_digits(x))
+        # print("----removed urls tags, set lowercase, remove specials characters and digits----\n", comments.head(5))
+        #
+        # # Tokenize comments
+        # comments = comments.apply(word_tokenize)
+        # print("----tokenized----\n", comments.head(12))
+        #
         # comments = lemmatize(comments)
         # print("----lemmatized----\n", comments.head(12))
-        #
-        # # Remove Stopwords
-        # stop_words = set(stopwords.words('english'))
-        # comments = comments.apply(lambda x: [item for item in x if item not in stop_words])
-        # print("----removed stop words----\n", comments.head(5))
+        # #
+        # # # Remove Stopwords
+        # # stop_words = set(stopwords.words('english'))
+        # # comments = comments.apply(lambda x: [item for item in x if item not in stop_words])
+        # # print("----removed stop words----\n", comments.head(5))
+        # #
         #
         # comments = remove_repeat(comments)
         # print("----removed repeat----\n", comments.head(5))
 
+        train['comments'] = train['comments'].apply(clean_text)
+        comments = train['comments']
+        comments.to_csv('test_processed.csv')
         return comments
 
 
 # How to use
-# X = TextPreprocess.process("reddit_train.csv")
+TextPreprocess.process("reddit_test.csv")
 
 
 
