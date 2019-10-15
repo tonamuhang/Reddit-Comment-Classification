3520052762299af7dab5a632ef8aae9a676411e0
Muhang <limuhang1@gmail.com>
2019-10-15 18:04:47 -0400

    CNN

---
+++ skipngram.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+from numpy import array
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from keras.layers import Flatten
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+train = pd.read_csv("reddit_train.csv", sep=',')
+# comments = train["comments"].astype(str)
+comments = pd.read_csv("train_processed.csv", sep=',').astype(str)
+labels = train["subreddits"].astype(str)
+
+# The maximum number of words to be used. (most frequent)
+MAX_NB_WORDS = 60000
+# Max number of words in each comments
+MAX_SEQUENCE_LENGTH = 300
+# This is fixed.
+EMBEDDING_DIM = 100
+tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+tokenizer.fit_on_texts(comments['comments'].values)
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+X = tokenizer.texts_to_sequences(comments['comments'].values)
+X = pad_sequences(X, maxlen=MAX_SEQUENCE_LENGTH)
+print('Shape of data tensor:', X.shape)
+Y = pd.get_dummies(train['subreddits']).values
+print('Shape of label tensor:', Y.shape)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.10, random_state = 1)
+print(X_train.shape,Y_train.shape)
+print(X_test.shape,Y_test.shape)
+
+model = Sequential()
+model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
+model.add(SpatialDropout1D(0.2))
+model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(20, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+epochs = 6
+batch_size = 128
+
+
+checkpointer = ModelCheckpoint(monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+
+history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size,validation_split=0.1,
+                    callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
+
+accr = model.evaluate(X_test,Y_test)
+print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
+
+X_test = pd.read_csv("submit_processed.csv", sep=',').astype(str)
+model.load_weights('weights.hdf5')
+predicted = model.predict(X_test)
+pd.DataFrame(predicted, columns=['Category']).to_csv('oct14_07pm.csv')
